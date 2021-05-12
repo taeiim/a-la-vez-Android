bdc839a75e2602f53cb927a8514be519ec4b2dcd
dlswer23 <lbj4298@naver.com>
2021-05-12 11:07:06 +0900

    [All]

---
+++ app/src/main/java/com/example/a_la_vez/dms/ui/activity/ForgetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a_la_vez.dms.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a_la_vez.R
+
+class ForgetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+    }
+}
--- app/src/main/java/com/example/a_la_vez/dms/ui/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/a_la_vez/dms/ui/activity/SignUpActivity.kt
@@ -1,16 +1,18 @@
 package com.example.a_la_vez.dms.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a_la_vez.R
 import retrofit2.Retrofit
 
 class SignUpActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
-
 
 
     }
