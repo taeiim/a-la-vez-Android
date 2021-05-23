c7277f093c1a335fcd181bd7789dc77cc87a1160
dlswer23 <lbj4298@naver.com>
2021-05-24 08:54:42 +0900

    [ALL] login만

--- app/src/main/java/com/example/a_la_vez/dms/ui/activity/SignInActivity.kt
+++ app/src/main/java/com/example/a_la_vez/dms/ui/activity/SignInActivity.kt
@@ -5,93 +5,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import com.example.a_la_vez.R
 import com.example.a_la_vez.dms.API.ApiClient
 import com.example.a_la_vez.dms.model.UserLogin
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_in.login_btn
+import kotlinx.android.synthetic.main.activity_sign_up.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import splitties.activities.start
+import kotlin.Unit.toString
 
 class SignInActivity : AppCompatActivity() {
 
     private var pressedTime: Int = 0
+    private lateinit var toast: Toast
+    private var email_et = findViewById<EditText>(R.id.email_et)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-
-        //비밀번호 찾기
-        forgetpassword_tv.setOnClickListener() {
-            val intent = Intent(this, ForgetPasswordActivity::class.java)
-            startActivity(intent)
-        }
-
-        //로그인
         login_btn.setOnClickListener() {
-            login()
+            if (email_et.text.toString() == "" || password_et.text.toString() == "") {
+                val Toast = Toast.makeText(this, "공백 없이 입력하세요", Toast.LENGTH_SHORT)
+                Toast.show()
+            } else if (email_et.text.toString().length < 15 || password_et.text.toString().length < 15) {
+                val Toast = Toast.makeText(this, "형식에 맞게 입력하세요", Toast.LENGTH_SHORT)
+                Toast.show()
+            }
+            //로그인 형식이 맞음
+            else {
+                val login = UserLogin(email_et.toString(), password_et.toString())
+                val loginInterface = ApiClient()
+                val Toast = Toast.makeText(this, "로그인에 성공하셨습니다", Toast.LENGTH_SHORT)
+                intent()
+                toast.show()
+
+            }
         }
     }
 
     private fun intent() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
-    }
-
-    private fun login() {
-
-        //공백일 때
-        if (email_et.text.toString() == "" || password_et.text.toString() == "") {
-            val Toast = Toast.makeText(this, "공백 없이 입력하세요", Toast.LENGTH_SHORT)
-            Toast.show()
-        } else if (email_et.text.toString().length < 15 || password_et.text.toString().length < 15) {
-            val Toast = Toast.makeText(this, "형식에 맞게 입력하세요", Toast.LENGTH_SHORT)
-            Toast.show()
-        }
-        //로그인 형식 맞을
-        else {
-            val login = UserLogin(email_et.toString(), password_et.toString())
-            val loginInterface = ApiClient()
-
-
-//            Call.enqueue(object : Callback<Void> {
-//                override fun onResponse(call: Call<Void>, response: Response<Void>) {
-//                    if (response.code() == 200) {
-//                        Toast.makeText(applicationContext, "로그인에 성공하였습니다", Toast.LENGTH_SHORT)
-//                                .show()
-//                        intent()
-//
-//                    } else if (response.code() == 400) {
-//                        Toast.makeText(applicationContext, "로그인 실패", Toast.LENGTH_SHORT)
-//                                .show()
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<Void>, t: Throwable) {
-//                    Log.d("dsfsf",toString())
-//                }
-//            })
-        }
-    }
-
-    override fun onBackPressed() {
-        if (pressedTime == 0) {
-            Toast.makeText(this, " 한 번 더 누르면 종료됩니다.", Toast.LENGTH_LONG).show();
-            pressedTime = System.currentTimeMillis().toInt();
-        } else {
-            val seconds = (System.currentTimeMillis() - pressedTime);
-
-            if (seconds > 2000) {
-                Toast.makeText(this, " 한 번 더 누르면 종료됩니다.", Toast.LENGTH_LONG).show();
-                pressedTime = 0;
-            } else {
-                super.onBackPressed();
-                finish(); // app 종료 시키기
-            }
-        }
     }
 }
