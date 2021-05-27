00b1b73491da551ffd62d4ceca27af8bbae95bf9
dlswer23 <lbj4298@naver.com>
2021-05-27 23:44:12 +0900

    [MODIFY] ui

--- app/src/main/java/com/example/a_la_vez/dms/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/a_la_vez/dms/ui/fragment/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.a_la_vez.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
@@ -17,5 +18,10 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
     }
 }
